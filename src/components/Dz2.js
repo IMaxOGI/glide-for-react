250c52a64b2a3faae88d62afa2088840be3b89b1
IMaxOGI <57328875+IMaxOGI@users.noreply.github.com>
2021-01-14 18:29:41 +0200

    Update

--- src/components/Dz2.js
+++ src/components/Dz2.js
@@ -1,63 +1,154 @@
-import React, { Component } from 'react';
-import '../Dz2.css'
+import React, { useState } from "react";
+import "../Dz2.css";
 import Glide from "./Glide";
-import { Checkbox, Container, Radio, Form, Input, Button } from "semantic-ui-react";
+import {
+  Checkbox,
+  Container,
+  Radio,
+  Form,
+  Input,
+  Button,
+} from "semantic-ui-react";
 
-class Dz2 extends Component {
+export default function Dz2() {
+  const [autoplay, setAutoplay] = useState(false);
+  const [type, setType] = useState("carousel");
+  const [perView, setPerView] = useState(3);
+  const [visible, setVisible] = useState(true);
 
-  state = {
-    autoplay: false,
-    type: 'carousel',
-    perView: 3,
-    visible: true
-  };
-
-  render() {
-    const { autoplay, type, perView } = this.state;
-    return (
-      <Container className='dz2'>
-        <Form>
-          <Form.Field>
-            <Input value={perView} type='numeric' onChange={(e) => this.setState({ perView: e.target.value })} />
-          </Form.Field>
-          <Form.Field>
-            <Checkbox label='Autoplay' checked={autoplay} onChange={() => this.setState({ autoplay: !autoplay })} />
-          </Form.Field>
-          <Form.Field>
-            <Radio
-              label='Carousel'
-              name='type'
-              value='carousel'
-              checked={type === 'carousel'}
-              onChange={() => this.setState({ type: 'carousel' })}
-            />
-          </Form.Field>
-          <Form.Field>
-            <Radio
-              label='Slider'
-              name='type'
-              value='slider'
-              checked={type === 'slider'}
-              onChange={() => this.setState({ type: 'slider' })}
-            />
-          </Form.Field>
-        </Form>
-        <Button onClick={() => this.setState({ visible: false })}>Remove Slider</Button>
-        {
-          this.state.visible &&
-          <Glide options={{autoplay: autoplay ? 2000 : false, type, perView}}>
-            <img className="full-with-img"
-                 src="https://images.wallpaperscraft.ru/image/kot_kosmonavt_skafandr_130111_1920x1080.jpg"
-                 alt=""/>
-            <img className="full-with-img" src="https://images.wallpaperscraft.ru/image/kot_milyj_shar_127642_1920x1080.jpg" alt=""/>
-            <img className="full-with-img"
-                 src="https://images.wallpaperscraft.ru/image/kot_risunok_art_apofiss_94604_1920x1080.jpg"
-                 alt=""/>
-          </Glide>
-        }
-      </Container>
-    );
-  }
+  return (
+    <Container className="dz2">
+      <Form>
+        <Form.Field>
+          <Input
+            value={perView}
+            type="numeric"
+            onChange={(e) => setPerView(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox
+            label="Autoplay"
+            checked={autoplay}
+            onChange={() => setAutoplay(!autoplay)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <Radio
+            label="Carousel"
+            name="type"
+            value="carousel"
+            checked={type === "carousel"}
+            onChange={() => setType("carousel")}
+          />
+        </Form.Field>
+        <Form.Field>
+          <Radio
+            label="Slider"
+            name="type"
+            value="slider"
+            checked={type === "slider"}
+            onChange={() => setType("slider")}
+          />
+        </Form.Field>
+      </Form>
+      <Button onClick={() => setVisible(false)}>Remove Slider</Button>
+      {visible && (
+        <Glide options={{ autoplay: autoplay ? 2000 : false, type, perView }}>
+          <img
+            className="full-with-img"
+            src="https://images.wallpaperscraft.ru/image/kot_kosmonavt_skafandr_130111_1920x1080.jpg"
+            alt=""
+          />
+          <img
+            className="full-with-img"
+            src="https://images.wallpaperscraft.ru/image/kot_milyj_shar_127642_1920x1080.jpg"
+            alt=""
+          />
+          <img
+            className="full-with-img"
+            src="https://images.wallpaperscraft.ru/image/kot_risunok_art_apofiss_94604_1920x1080.jpg"
+            alt=""
+          />
+        </Glide>
+      )}
+    </Container>
+  );
 }
+// class Dz2 extends Component {
+//   state = {
+//     autoplay: false,
+//     type: "carousel",
+//     perView: 3,
+//     visible: true,
+//   };
 
-export default Dz2;
+//   render() {
+//     const { autoplay, type, perView } = this.state;
+//     console.log(autoplay);
+//     console.log(type);
+//     console.log(perView);
+//     return (
+//       <Container className="dz2">
+//         <Form>
+//           <Form.Field>
+//             <Input
+//               value={perView}
+//               type="numeric"
+//               onChange={(e) => this.setState({ perView: e.target.value })}
+//             />
+//           </Form.Field>
+//           <Form.Field>
+//             <Checkbox
+//               label="Autoplay"
+//               checked={autoplay}
+//               onChange={() => this.setState({ autoplay: !autoplay })}
+//             />
+//           </Form.Field>
+//           <Form.Field>
+//             <Radio
+//               label="Carousel"
+//               name="type"
+//               value="carousel"
+//               checked={type === "carousel"}
+//               onChange={() => this.setState({ type: "carousel" })}
+//             />
+//           </Form.Field>
+//           <Form.Field>
+//             <Radio
+//               label="Slider"
+//               name="type"
+//               value="slider"
+//               checked={type === "slider"}
+//               onChange={() => this.setState({ type: "slider" })}
+//             />
+//           </Form.Field>
+//         </Form>
+//         <Button onClick={() => this.setState({ visible: false })}>
+//           Remove Slider
+//         </Button>
+//         {this.state.visible && (
+//           <Glide options={{ autoplay: autoplay ? 2000 : false, type, perView }}>
+//             <img
+//               className="full-with-img"
+//               src="https://images.wallpaperscraft.ru/image/kot_kosmonavt_skafandr_130111_1920x1080.jpg"
+//               alt=""
+//             />
+//             <img
+//               className="full-with-img"
+//               src="https://images.wallpaperscraft.ru/image/kot_milyj_shar_127642_1920x1080.jpg"
+//               alt=""
+//             />
+//             <img
+//               className="full-with-img"
+//               src="https://images.wallpaperscraft.ru/image/kot_risunok_art_apofiss_94604_1920x1080.jpg"
+//               alt=""
+//             />
+//           </Glide>
+//         )}
+//       </Container>
+//     );
+//   }
+// }
+
+// export default Dz2;
--- src/components/Glide.js
+++ src/components/Glide.js
@@ -1,44 +1,81 @@
-import React, { Component } from 'react'
-import GlideJs from '@glidejs/glide'
-import '@glidejs/glide/dist/css/glide.core.min.css'
+import React, { useEffect, useRef } from "react";
+import GlideJs from "@glidejs/glide";
+import "@glidejs/glide/dist/css/glide.core.min.css";
 import "@glidejs/glide/dist/css/glide.theme.min.css";
 
-export default class Glide extends Component {
+export default function Glide(props) {
+  const slider = useRef();
+  const glider = useRef(null);
 
-    constructor(props) {
-        super(props);
-        this.slider = React.createRef();
-        this.glider = null;
-    }
+  useEffect(() => {
+    glider.current = new GlideJs(slider.current, props.options).mount();
+    // console.log(glider);
+    // console.log(glider.current);
+    return () => {
+      glider.current.destroy();
+    };
+  }, [props.options]);
 
-    componentDidMount() {
-        this.glider = new GlideJs(this.slider.current, this.props.options).mount()
-    }
-
-    componentDidUpdate(prevProps) {
-        if(this.props.options !== prevProps.options) {
-            this.glider.update(this.props.options)
-        }
-    }
-
-    componentWillUnmount() {
-        this.glider.destroy()
-    }
-
-    render() {
-        const { children } = this.props;
-        return (
-            <div ref={this.slider} className="glide">
-                <div className="glide__track" data-glide-el="track">
-                    <ul className="glide__slides">
-                        {children.map(img =>  <li className="glide__slide">{img}</li>)}
-                    </ul>
-                </div>
-                <div className="glide__arrows" data-glide-el="controls">
-                    <button className="glide__arrow glide__arrow--left" data-glide-dir="<">prev</button>
-                    <button className="glide__arrow glide__arrow--right" data-glide-dir=">">next</button>
-                </div>
-            </div>
-        )
-    }
+  return (
+    <div ref={slider} className="glide">
+      <div className="glide__track" data-glide-el="track">
+        <ul className="glide__slides">
+          {props.children.map((img, i) => (
+            <li className="glide__slide" key={i}>
+              {img}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="glide__arrows" data-glide-el="controls">
+        <button className="glide__arrow glide__arrow--left" data-glide-dir="<">
+          prev
+        </button>
+        <button className="glide__arrow glide__arrow--right" data-glide-dir=">">
+          next
+        </button>
+      </div>
+    </div>
+  );
 }
+
+// export default class Glide extends Component {
+
+//     constructor(props) {
+//         super(props);
+//         this.slider = React.createRef();
+//         this.glider = null;
+//     }
+
+//     componentDidMount() {
+//         this.glider = new GlideJs(this.slider.current, this.props.options).mount()
+//         console.log(this.props.options, this.glider)
+//     }
+
+//     componentDidUpdate(prevProps) {
+//         if(this.props.options !== prevProps.options) {
+//             this.glider.update(this.props.options)
+//         }
+//     }
+
+//     componentWillUnmount() {
+//         this.glider.destroy()
+//     }
+
+//     render() {
+//         const { children } = this.props;
+//         return (
+//             <div ref={this.slider} className="glide">
+//                 <div className="glide__track" data-glide-el="track">
+//                     <ul className="glide__slides">
+//                         {children.map(img =>  <li className="glide__slide">{img}</li>)}
+//                     </ul>
+//                 </div>
+//                 <div className="glide__arrows" data-glide-el="controls">
+//                     <button className="glide__arrow glide__arrow--left" data-glide-dir="<">prev</button>
+//                     <button className="glide__arrow glide__arrow--right" data-glide-dir=">">next</button>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
